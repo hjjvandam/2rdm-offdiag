d1485638d46e61e245c43cfbcd246b3b9affab53
Hubertus van Dam <hubertus.vandam@uni-due.de>
2025-10-04 23:19:49 +0200

    Script to (re)run all calculations

---
+++ cases/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+folders=(h2_3.0_sto6g h2_4.0_sto6g h2_5.0_sto6g h2_6.0_sto6g h2_12.0_sto6g
+         h4_3.0_sto6g h4_4.0_sto6g h4_5.0_sto6g h4_6.0_sto6g h4_12.0_sto6g
+         h6_3.0_sto6g h6_4.0_sto6g h6_5.0_sto6g h6_6.0_sto6g h6_12.0_sto6g
+         h8_3.0_sto6g h8_4.0_sto6g h8_5.0_sto6g h8_6.0_sto6g h8_12.0_sto6g)
+path=`pwd`
+for folder in "${folders[@]}"; do
+    echo $folder
+    cd $path/$folder
+    ./run_neutral.sh 2>&1 | tee run_neutral.log
+done
